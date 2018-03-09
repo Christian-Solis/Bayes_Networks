de73482a48abc1d3d4f5793bb36ca65ac9e74bba
Christian-Solis <35510701+Christian-Solis@users.noreply.github.com>
2018-03-09 16:55:02 -0600

    Initial Commit

---
+++ bayes-network.py
@@ -0,0 +1,7 @@
+# -----------------------------------------------------------------------------
+# Christian Ricardo Solís Cortés & Eduardo Josué Contreras Álvarez
+# A01063685
+# Artificial Intelligence
+# Assignment: Bayes Networks
+# Querying a Bayes Network
+# -----------------------------------------------------------------------------
