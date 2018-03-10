9039c076f5b294bc30ce66d8a159a593b90a5c82
Christian-Solis <35510701+Christian-Solis@users.noreply.github.com>
2018-03-09 19:01:05 -0600

    Start of parser

--- bayes-network.py
+++ bayes-network.py
@@ -5,3 +5,35 @@
 # Assignment: Bayes Networks
 # Querying a Bayes Network
 # -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# Parsing and cleaning process
+# -----------------------------------------------------------------------------
+
+# Clean initial string to get only the nodes
+
+def input_reading(initial_input):
+    input_without_spaces = initial_input.replace(' ', '')
+
+    # cont_stacks = list()
+    # # split when a ';' is found
+    # stacks = input_without_spaces.split(';')
+    # for st in stacks:
+    #     st = st.replace('(', '')
+    #     st = st.replace(')', '')
+    #     if st == '':
+    #         cont_stacks.append(list())
+    #     else:
+    #         containers = st.split(',')
+    #         cont_stacks.append(containers)
+    # return cont_stacks
+
+def main():
+    bayes = input()
+    input_reading(input)
+
+    # read_probability_tables(bayes_net)
+    # read_execute_queries(bayes_net)
+
+if __name__ == '__main__':
+    main()
