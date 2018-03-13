e0a209b957140d653b291652b45c0b1ada647ee4
Christian-Solis <35510701+Christian-Solis@users.noreply.github.com>
2018-03-12 23:17:26 -0600

    Parser complete

--- bayes-network.py
+++ bayes-network.py
@@ -6,34 +6,47 @@
 # Querying a Bayes Network
 # -----------------------------------------------------------------------------
 
+import sys
+import heapq
+import itertools
+
 # -----------------------------------------------------------------------------
 # Parsing and cleaning process
 # -----------------------------------------------------------------------------
 
-# Clean initial string to get only the nodes
-
+# Clean initial string to get only the necessary info
 def input_reading(initial_input):
-    input_without_spaces = initial_input.replace(' ', '')
+    # Remove spaces from the input
+    input_without_spaces = initial_input.read().replace(' ', '')
+    # Split input by line breaks
+    splited_input_by_line_break = input_without_spaces.split('\n')
+    # Save nodes and number of probabilities in stack
+    stack_of_nodes = splited_input_by_line_break[0]
+    number_of_probabilities = int(splited_input_by_line_break[1])
+    # Save probabilities in a list
+    probabilities = list()
+    for line in range(2, 2 + number_of_probabilities):
+        probabilities.append(splited_input_by_line_break[line])
+    # Save number of queries in stack
+    number_of_queries = int(splited_input_by_line_break[2 + number_of_probabilities])
+    # Save queries in a list
+    queries = list()
+    for line in range(3 + number_of_probabilities, 3 + number_of_probabilities + number_of_queries):
+        queries.append(splited_input_by_line_break[line])
 
-    # cont_stacks = list()
-    # # split when a ';' is found
-    # stacks = input_without_spaces.split(';')
-    # for st in stacks:
-    #     st = st.replace('(', '')
-    #     st = st.replace(')', '')
-    #     if st == '':
-    #         cont_stacks.append(list())
-    #     else:
-    #         containers = st.split(',')
-    #         cont_stacks.append(containers)
-    # return cont_stacks
+    print(stack_of_nodes)
+    print(number_of_probabilities)
+    print(probabilities)
+    print(number_of_queries)
+    print(queries)
+
+# -----------------------------------------------------------------------------
+# Main function
+# -----------------------------------------------------------------------------
 
 def main():
-    bayes = input()
-    input_reading(input)
-
-    # read_probability_tables(bayes_net)
-    # read_execute_queries(bayes_net)
+    bayes = open('test.in','r')
+    input_reading(bayes)
 
 if __name__ == '__main__':
     main()
